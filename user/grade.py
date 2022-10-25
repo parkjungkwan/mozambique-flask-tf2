80358b115b27f30159115c1d8ae30305846a2827
parkjungkwan <pakjkwan@gmail.com>
2022-10-25 15:07:31 +0900

    15

---
+++ user/grade.py
@@ -0,0 +1,7 @@
+from util.common import Common
+
+
+class Contacts(object):
+    def __init__(self):
+        pass
+
---
+++ util/common.py
@@ -0,0 +1,9 @@
+class Common(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def menu(ls):
+        for i, j in enumerate(ls): # ["종료", "등록", "출력", "삭제"]
+            print(f"{i}. {j}")
+        return int(input("메뉴 선택: "))

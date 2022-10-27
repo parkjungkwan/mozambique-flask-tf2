e07f63aa879f805b9c9682676287a9c61edce388
parkjungkwan <pakjkwan@gmail.com>
2022-10-27 18:07:50 +0900

    final

--- titanic/models.py
+++ titanic/models.py
@@ -32,10 +32,18 @@ class TitanicModel(object):
 
     @staticmethod
     def create_train(this)->object:
-        pass
+        return this.train.drop('Survived', axis = 1)
 
-    def create_label(self):
-        pass
+    @staticmethod
+    def create_label(this) -> object:
+        return this.train['Survived']
+
+    @staticmethod
+    def drop_features(this, *feature) -> object:
+        for i in feature:
+            this.train = this.train.drop(i, axis = 1)
+            this.test = this.test.drop(i, axis = 1)
+        return this
 
 if __name__ == '__main__':
     t = TitanicModel()

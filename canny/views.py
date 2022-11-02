8948eda2b2698d1656c42b4be7574c67e48df5d0
parkjungkwan <pakjkwan@gmail.com>
2022-11-02 14:07:25 +0900

    14

---
+++ canny/views.py
@@ -0,0 +1,50 @@
+from matplotlib import pyplot as plt
+from PIL import Image
+from canny.models import ImageToNumberArray, GaussianBlur, Canny, image_read
+import cv2 as cv
+import numpy as np
+class MenuController(object):
+    @staticmethod
+    def menu_0(*params):
+         print(params[0])
+
+    @staticmethod
+    def menu_1(*params):
+        print(params[0])
+        img = image_read(params[1])
+        print(f'cv2 버전 {cv.__version__}')  # cv2 버전 4.6.0
+        print(f' Shape is {img.shape}')
+        cv.imshow('Gray', img)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+    @staticmethod
+    def menu_2(*params):
+        print(params[0])
+        arr = ImageToNumberArray(params[1])
+        # 람다식 내부에서 GRAYSCALE 변환 공식 사용함
+        img = (lambda x: x[:, :, 0] * 0.114 + x[:, :, 1] * 0.587 + x[:, :, 2] * 0.229)(arr)
+        img = GaussianBlur(img, 1, 1)
+        img = Canny(img, 50, 150)
+        plt.imshow((lambda x: Image.fromarray(x))(img))
+        plt.show()
+
+
+    @staticmethod
+    def menu_3(*params):
+        print(params[0])
+        ### 디스크에서 읽는 경우 ###
+        # img = cv.imread('./data/roi.jpg', 0)
+        # img = cv.imread(img, 0)
+        ### 메모리에서 읽는 경우 ###
+        img = ImageToNumberArray(params[1])
+        print(f'img type : {type(img)}')
+        edges = cv.Canny(np.array(img), 100, 200)
+        plt.subplot(121), plt.imshow(img, cmap='gray')
+        plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+        plt.subplot(122), plt.imshow(edges, cmap='gray')
+        plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+        plt.show()
+
+
+

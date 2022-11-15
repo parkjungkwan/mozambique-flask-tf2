3cede50b435c1f701f0f2a1cddfbc8813e36cdf5
parkjungkwan <pakjkwan@gmail.com>
2022-11-15 18:02:04 +0900

    18

--- ml/stroke.py
+++ ml/stroke.py
@@ -2,15 +2,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder
 from imblearn.under_sampling import RandomUnderSampler
-STROKE_MENUS = ["종료", #0
-                "데이터구조파악",#1
-                "변수한글화",#2
-                "연속형변수편집",#3 18세이상만 사용함
-                "범주형변수편집",#4
-                "샘플링",#5
-                "모델링",#6
-                "학습",#7
-                "예측"]#8
+STROKE_MENUS = ["Exit", #0
+                "Spec",#1
+                "Rename",#2
+                "Inteval",#3 18세이상만 사용함
+                "Norminal",#4
+                "Target",#5
+                "Partition",#6
+                "미완성: Fit",#7
+                "미완성: Predicate"]#8
 stroke_meta = {
     'id':'아이디', 'gender':'성별', 'age':'나이', 
     'hypertension':'고혈압',
@@ -28,10 +28,11 @@ stroke_menu = {
     "2" : lambda t: t.rename_meta(),
     "3" : lambda t: t.interval_variables(),
     "4" : lambda t: t.categorical_variables(),
-    "5" : lambda t: t.sampling(),
-    "6" : lambda t: print(" ** No Function ** "),
+    "5" : lambda t: t.partition(),
+    "6" : lambda t: t.partition(),
     "7" : lambda t: print(" ** No Function ** "),
     "8" : lambda t: print(" ** No Function ** "),
+    "9" : lambda t: print(" ** No Function ** "),
 
 }
 '''
@@ -153,7 +154,7 @@ class StrokeService:
         print(f'--- data shape --- \n {self.data}')
         print(f'--- target shape --- \n {self.target}')
 
-    def sampling(self):
+    def partition(self):
         data = self.data
         target = self.target
         undersample = RandomUnderSampler(sampling_strategy=0.333, random_state=2)

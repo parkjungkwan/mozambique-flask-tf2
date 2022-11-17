c18128442652ffc4017ba4280f5bfa34d2421485
parkjungkwan <pakjkwan@gmail.com>
2022-11-17 16:34:33 +0900

    16

--- ml/crime.py
+++ ml/crime.py
@@ -1,6 +1,7 @@
 import googlemaps
 import numpy as np
 import pandas as pd
+from sklearn import preprocessing
 
 CRIME_MENUS = ["Exit", #0
                 "Show Spec",#1
@@ -52,6 +53,9 @@ class Crime:
         self.cctv = pd.read_csv('./data/cctv_in_seoul.csv')
         self.pop = pd.read_excel('./data/pop_in_seoul.xls',usecols=["자치구","합계","한국인","등록외국인","65세이상고령자"], skiprows=[0,2])
         self.ls = [self.crime, self.cctv, self.pop]
+        self.crime_rate_columns = ['살인검거율', '강도검거율', '강간검거율', '절도검거율', '폭력검거율']
+        self.crime_columns = ['살인', '강도', '강간', '절도', '폭력']
+        self.arrest_columns = ['살인 검거', '강도 검거', '강간 검거', '절도 검거', '폭력 검거']
 
     '''
     1.스펙보기 
@@ -121,7 +125,6 @@ class Crime:
             pop.columns[3]: '외국인',
             pop.columns[4]: '고령자'
         }, inplace=True)
-        print("*"*100)
         pop.drop(index=26,inplace=True)
         pop['외국인비율'] = pop['외국인'].astype(int) / pop['인구수'].astype(int) * 100
         pop['고령자비율'] = pop['고령자'].astype(int) / pop['인구수'].astype(int) * 100
@@ -153,19 +156,37 @@ class Crime:
 
     def save_police_norm(self):
         police_pos = pd.read_pickle('./save/police_pos.pkl')
-        print(police_pos)
-        print("/"*100)
         police = pd.pivot_table(police_pos,index="구별",aggfunc=np.sum)
-        print(police)
         police['살인검거율'] = (police['살인 검거'].astype(int) / police['살인 발생'].astype(int)) * 100
         police['강도검거율'] = (police['강도 검거'].astype(int) / police['강도 발생'].astype(int)) * 100
         police['강간검거율'] = (police['강간 검거'].astype(int) / police['강간 발생'].astype(int)) * 100
         police['절도검거율'] = (police['절도 검거'].astype(int) / police['절도 발생'].astype(int)) * 100
         police['폭력검거율'] = (police['폭력 검거'].astype(int) / police['폭력 발생'].astype(int)) * 100
         police.drop(columns={'살인 검거','강도 검거','강간 검거','절도 검거','폭력 검거'}, axis=1, inplace=True)
+        for i in self.crime_rate_columns:
+            police.loc[police[i] > 100, 1] = 100 # 데이터값의 기간 오류로 100을 넘으면 100으로 계산
+        police.rename(columns={
+            '살인 발생': '살인',
+            '강도 발생': '강도',
+            '강간 발생': '강간',
+            '절도 발생': '절도',
+            '폭력 발생': '폭력'
+        }, inplace=True)
+        x = police[self.crime_rate_columns].values
+        min_max_scalar = preprocessing.MinMaxScaler()
+        """
+        스케일링은 선형변환을 적용하여
+        전체 자료의 분포를 평균 0, 분산 1이 되도록 만드는 과정
+        """
+        x_scaled = min_max_scalar.fit_transform(x.astype(float))
+        """
+        정규화 normalization
+        많은 양의 데이터를 처리함에 있어 데이터의 범위(도메인)를 일치시키거나
+        분포(스케일)를 유사하게 만드는 작업
+        """
+        police_norm = pd.DataFrame(x_scaled, columns=self.crime_columns, index=police.index)
+        print(police_norm)
 
-        for i in []:
-            police.loc[0] # 데이터값의 기간 오류로 100을 넘으면 100으로 계산
 
     def norminal(self):
         pass

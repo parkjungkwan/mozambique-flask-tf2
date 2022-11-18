a40046c37c1d63a65072c1a1cbace419ae1c04d5
parkjungkwan <pakjkwan@gmail.com>
2022-11-18 17:38:56 +0900

    17

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    a = {"a": "b", "c": "d"}
-    return a
+    return "Hello World !!"
+
+@app.route("/users/<username>")
+def get_user(username):
+    return username
+
+@app.route("/posts/<int:post_id>")
+def get_post(post_id):
+    return str(post_id)
+
+@app.route("/uuid/<uuid:uuid>")
+def get_uuid(uuid):
+    return str(uuid)
+
+@app.route("/login", methods=["GET","POST"])
+def login_page():
+    if request.method == "POST":
+        return "로그인 성공"
+    else:
+        return "로그인 화면"
+
 
 if __name__=="__main__":
     app.run()
---
+++ ml/tensor_one.py
@@ -0,0 +1,3 @@
+import tensorflow as tf
+if __name__ == '__main__':
+    print(f"텐서버전: {tf.__version__}")
